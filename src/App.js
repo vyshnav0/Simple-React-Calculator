1cc0cc73c6346958ffc33e27c81c5cd39a5523f5
Vyshnav <vyshnavk78@gmail.com>
2023-09-04 15:55:22 +0530

    removed unwanted file, add basic buttons & styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">  {/*  the outermost  */}
+      <div className="my-calc">My Calculator</div>
+      <div className="output-screen">  {/* the display screen */}
+        <div className="prev-operand"></div>  {/* the prev output that is gets pushed up when we enter another operand */}
+        <div className="curr-operand"></div>  {/* the operand we enter after prev-operand */}
+      </div>
+
+      <button className="spans">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>×</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>-</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>+</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="spans">=</button>
+
     </div>
   );
 }
